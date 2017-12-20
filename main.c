6e77c9a1890a04d60fe0c5f3de4c7523dc0bc802
chakilo <konarchen@live.com>
2017-12-20 16:20:33 +0800

    init

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void usart_0_rec(uint8_t data) {
 
 int main(void) {
 
+    // init
     usart_init(USART_0, F_CPU, 115200, 0x00, SETB(RXCIE1) | SETB(TXEN1) | SETB(RXEN1), SETB(UCSZ11) | SETB(UCSZ10), usart_0_rec);
 
     while (1) {
